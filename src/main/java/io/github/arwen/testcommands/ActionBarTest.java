00a3e55d533409b1ff16dec1cb2adc7b2097fff0
Alexander Milanovich <steave0982@outlook.com>
2019-03-17 15:34:24 -0400

    Working Actionbar
    Signed-off-by: Steave0982 <Steave0982@outlook.com>

--- src/main/java/io/github/arwen/testcommands/ActionBarTest.java
+++ src/main/java/io/github/arwen/testcommands/ActionBarTest.java
@@ -1,4 +1,25 @@
 package io.github.arwen.testcommands;
 
-public class ActionBarTest {
+import io.github.arwen.actionbar.ActionBar;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ActionBarTest implements CommandExecutor {
+
+    public String actionbar = ChatColor.RED  + "Testing";
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+
+                ActionBar.send(player, actionbar);
+
+        }
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
 }
